5e8f80b9eee2ec5853d7618e20d66f592636aba2
Cahid <mr.cebizade@gmail.com>
2023-10-17 07:13:19 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - writes the character c to stdout
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while(a[i] != '\0')
+	{
+		_putchar(a[i]);
+		i++;
+	}
+	_putchar('\n');
+}
