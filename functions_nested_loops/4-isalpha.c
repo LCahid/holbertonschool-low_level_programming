89d1d2e23a00336b876323752d762834a6e8cad3
Cahid <mr.cebizade@gmail.com>
2023-10-18 03:55:43 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - writes the character c to stdout
+ *
+ * @c: var
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
