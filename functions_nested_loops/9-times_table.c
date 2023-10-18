ec81e4d901ee8e9892affe5bfccc97bda2dda0f9
Cahid <mr.cebizade@gmail.com>
2023-10-18 05:41:43 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,23 +17,20 @@ void times_table(void)
 		{
 			if ((i * j) >= 10)
 			{
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
-				if (j != 9)
-				{
 				_putchar(',');
 				_putchar(' ');
-				}
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
 			}
 			else
 			{
-				_putchar((i * j) + '0');
-				if (j != 9)
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar((i * j) + '0');
 			}
 		}
 		_putchar('\n');
