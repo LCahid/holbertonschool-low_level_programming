b23e53f0733e504e3961291f0f392be6aa9bdc84
Cahid <mr.cebizade@gmail.com>
2023-10-20 08:59:54 -0700

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - writes the character c to stdout
+ *
+ * @size: val
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{	
+			if (size - i - 1 > j)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+}
