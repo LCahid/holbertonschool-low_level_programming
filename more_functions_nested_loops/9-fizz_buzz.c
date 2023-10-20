5a000e736bc21f862d80a2b1e05952a601b8abd7
Cahid <mr.cebizade@gmail.com>
2023-10-20 08:23:18 -0700

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 3) && !(i % 5))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (!(i % 3))
+		{
+			printf("Fizz ");
+		}
+		else if (!(i % 5))
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
+
