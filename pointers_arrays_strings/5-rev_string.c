94fdf438bea0d83148428d835732a9920ecfbf04
Cahid <mr.cebizade@gmail.com>
2023-10-24 06:52:09 -0700

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+  * rev_string - reset to 98
+  *
+  * @s: str
+  * Return: length
+  */
+void rev_string(char *s)
+{
+	int i = 0;
+	int l = strlen(s);
+	char temp;
+
+	while (i < l/2)
+	{
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = temp;
+		i++;
+	}
+}
