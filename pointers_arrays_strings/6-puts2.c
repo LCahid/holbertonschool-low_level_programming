82d52df1dabc2c5be5448caaa153eb6e16c9ca05
Cahid <mr.cebizade@gmail.com>
2023-10-25 05:12:34 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+  * puts2 - reset to 98
+  *
+  * @str: str
+  * Return: length
+ */
+void puts2(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	for (i = 0; i < l; i++)
+	{
+		if (!(i % 2))
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
