6d0533a156790113d6782650a0bbea74b29fda69
Cahid <mr.cebizade@gmail.com>
2023-10-25 06:25:59 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+  * puts_half - reset to 98
+  *
+  * @str: str
+  * Return: length
+ */
+void puts_half(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	for (i = l / 2; i < l; i++)
+	{
+		if (l % 2)
+		{
+			i = (l - 1) / 2;
+		}
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
