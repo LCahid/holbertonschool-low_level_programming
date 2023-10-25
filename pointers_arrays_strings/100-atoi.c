b06d2f24571cb4203623e6c8c750b3a6eee26edb
Cahid <mr.cebizade@gmail.com>
2023-10-25 07:32:37 -0700

    100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * _atoi - reset to 98
+  *
+  * @dest: num
+  * @src: num
+  * Return: length
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1;
+	int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] == '+')
+		{
+			sign *= 1;
+		}
+		else if (s[i] <= 57 && s[i] >= 48)
+		{
+			num *= 10;
+			num += s[i] - 48;
+		}
+		else
+		{
+			if (num != 0)
+			{
+				break;
+			}
+		}
+		i++;
+	}
+	num *= sign;
+	return (num);
+}
