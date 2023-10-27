d8c8d4fa4e51c9a7f2ad1ab2a3ee8559a6dd2682
Cahid <mr.cebizade@gmail.com>
2023-10-27 08:49:15 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+  * cap_string - reset to 98
+  *
+  * @s: str
+  * Return: length
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i != 0)
+		{
+			if (s[i - 1] < 48)
+			{
+				if (s[i] >= 97 && s[i] <= 122)
+				{
+	 				s[i] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
