25bf9690b8b70beb28a5a8d0d87291f6f92691ed
Cahid <mr.cebizade@gmail.com>
2023-10-27 06:52:43 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * string_toupper - reset to 98
+  *
+  * @c: num
+  *
+  * Return: length
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
