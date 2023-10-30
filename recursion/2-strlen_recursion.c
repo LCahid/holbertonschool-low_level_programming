eed0fb40ec7154b46ba0e965e28720fae4da47b1
Cahid <mr.cebizade@gmail.com>
2023-10-30 04:54:19 -0700

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - fills memory
+ * @s: data
+ *
+ * Return: char
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != 0)
+	{
+		i++;
+		return (i + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
