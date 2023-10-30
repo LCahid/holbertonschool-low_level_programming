7c0f3f5de643b5e73ee33c4a1f9bf90ebba67824
Cahid <mr.cebizade@gmail.com>
2023-10-30 05:03:29 -0700

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * factorial - fills memory
- * @n: data
+ * _pow_recursion - fills memory
+ * @x: data
+ * @y: data
  *
  * Return: char
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n == 0)
-		return (0);
-	return (n + factorial(n - 1));
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
 }
