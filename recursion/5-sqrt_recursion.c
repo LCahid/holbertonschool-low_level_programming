7cf9808fcc0dabef06ffc715de5b197f6e1fd04b
Cahid <mr.cebizade@gmail.com>
2023-10-30 05:28:53 -0700

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,11 +11,11 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (find_sqrt(n, 1));
 }
-/** 
+/**
   * find_sqrt - find square root
   * @n: num
   * @k: num
-  * Retunr: square root
+  * Return: square root
   */
 int find_sqrt(int n, int k)
 {
