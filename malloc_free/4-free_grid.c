919bb7d434ccd99e72264f9c20420fec1a8b8f30
Cahid <mr.cebizade@gmail.com>
2023-11-06 04:40:19 -0800

    4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * free_grid - fills memory
+ * @grid: str
+ * @height: str
+ * Return: char
+ */
+void free_grid(int **grid, int height)
+{
+	int j;
+
+	for (j = height; j >= 0; j--)
+		free(grid[j]);
+	free(grid);
+}
