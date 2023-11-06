d1e801c4c38485d0f11164ae2542e6c13c81efbb
Cahid <mr.cebizade@gmail.com>
2023-11-06 03:09:26 -0800

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,11 +9,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
 	char *buf = malloc(sizeof(char) * size);
 	unsigned int i;
-	
+
+	if (size == 0)
+		return (NULL);
 	if (buf == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
