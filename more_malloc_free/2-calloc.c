d56e4b92ec2821494e71245cdaeb2c6f36c563f9
Cahid <mr.cebizade@gmail.com>
2023-11-13 06:29:55 -0800

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,11 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *buf;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	buf = malloc(nmemb * size);
 	if (buf == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		buf[nmemb] = 0;
 	return (buf);
 }
