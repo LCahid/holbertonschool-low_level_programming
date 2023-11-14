98ca057db213f462d4c76c7648d8b282e628e814
Cahid <mr.cebizade@gmail.com>
2023-11-14 06:15:32 -0800

    101

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -35,7 +35,7 @@ char **strtow(char *str)
 			s = i;
 			while (str[i] != 32 && str[i])
 				i++;
-			buf[j] = malloc(sizeof(char) * (i - s) + 1);
+			buf[j] = malloc(sizeof(char) * (i - s));
 			while (str[s] != 32 && str[s])
 			{
 				buf[j][e] = str[s];
