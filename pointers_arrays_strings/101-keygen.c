4271c0c593b84e837b84ad111e80a01ebd98ce35
Cahid <mr.cebizade@gmail.com>
2023-11-15 15:32:39 -0800

    101

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 int main(void){
-	int i, x= 0, y= 0;
+	int i, x= 0, y= 0, z= 0;
 	char c[29];
-	for (i=0;i<25;i++)
+	for (i=0;i<24;i++)
 		c[i] = 'c';
 	srand(time(NULL));
-	while ((x + y) != 198)
+	while ((x + y + z) != 269)
 	{
 		x = rand() % 127;
 		y = rand() % 127;
+		z = rand() % 127;
 	}
 	c[i++] = y;
 	c[i++] = x;
+	c[i++] = z;
 	c[i++] = 127;
 	c[i] = '\0';
 	printf("%s", c);
