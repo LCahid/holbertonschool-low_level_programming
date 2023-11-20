25d7438a170cf933713a90eb175cb98d2c66adb5
Cahid <mr.cebizade@gmail.com>
2023-11-20 05:04:22 -0800

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef H_H
 #define H_H
-struct dog{
+/**
+  * struct dog - dog
+  * @name: name
+  * @age: age
+  * @owner: owner
+  * Description: desc
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
