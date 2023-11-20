aacab3cb8154a67672d117c881e6c83865ae5288
Cahid <mr.cebizade@gmail.com>
2023-11-20 06:06:30 -0800

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * new_dog - init
+  * @name: name
+  * @age: age
+  * @owner: owner
+  * Return: new_dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int i, sizen, sizeo;
+
+	if (name == NULL)
+		sizen = 0;
+	else
+		sizen = sizeof(name);
+	if (owner == NULL)
+		sizeo = 0;
+	else
+		sizeo = sizeof(owner);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * sizen + 1);
+	dog->owner = malloc(sizeof(char) * sizeo + 1);
+	if (dog->name == NULL || dog->owner == NULL)
+		return (NULL);
+	for (i = 0; i < sizen + sizeo; i++)
+	{
+		if (i < sizen)
+			dog->name[i] = name[i];
+		else
+			dog->owner[i - sizen] = owner[i - sizen];
+	}
+	dog->age = age;
+	return (dog);
+}
