153ffe558cc3ba0998d53e2cde9ab824efc941d8
Cahid <mr.cebizade@gmail.com>
2023-11-21 00:39:21 -0800

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -7,8 +7,10 @@ int main(int argc, char **argv) {
     size_t bytes_written;
     FILE *file = fopen("cahid.sh", "w");
 
-    if (argc < 3)
-	    return (1);
+    if (argc != 2)
+	    return (98);
+    if (!isdigit(argv[1]) || !isdigit(argv[2]))
+	    return (98);
     if (file == NULL) {
         fprintf(stderr, "Error opening the file.\n");
         return (1);
@@ -16,6 +18,11 @@ int main(int argc, char **argv) {
 
     size = strlen(argv[1]) + strlen(argv[2]);
     data = malloc(size + 50);
+    if (data == NULL)
+    {
+	    free(data);
+	    return (98);
+    }
     sprintf(data, "#!/bin/bash\necho \"a = %s; b= %s; a*b;\" | bc", argv[1], argv[2]); 
 
     bytes_written = fwrite(data, 1, strlen(data), file);
@@ -29,7 +36,7 @@ int main(int argc, char **argv) {
     fclose(file);
     free(data);
     system("chmod u+x cahid.sh");
-    system("./cahid.sh | tr -d '\\\n'");
+    system("./cahid.sh | tr -d '\\\\\n'");
 
     return (0);
 }
