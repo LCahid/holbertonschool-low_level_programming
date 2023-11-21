e11f2882f00c9c6f872ffe2c1c49513efef73df9
Cahid <mr.cebizade@gmail.com>
2023-11-21 00:28:34 -0800

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char **argv) {
-    // Open a file for writing (creates a new file or truncates an existing one)
+int main(char **argv) {
+    int size;
+    char *data;
+    size_t bytes_written;
     FILE *file = fopen("cahid.sh", "w");
 
     if (file == NULL) {
@@ -10,17 +12,15 @@ int main(int argc, char **argv) {
         return 1; // Return an error code
     }
 
-    int size = strlen(argv[1]) + strlen(argv[2]);
-    char *data = malloc(size + 50);
+    size = strlen(argv[1]) + strlen(argv[2]);
+    data = malloc(size + 50);
     sprintf(data, "#!/bin/bash\necho \"a = %s; b= %s; a*b;\" | bc", argv[1], argv[2]); 
 
-    // Write the data to the file
-    size_t bytes_written = fwrite(data, 1, strlen(data), file);
+    bytes_written = fwrite(data, 1, strlen(data), file);
 
-    // Check if the write operation was successful
     if (bytes_written != strlen(data)) {
         fprintf(stderr, "Error writing to the file.\n");
-        fclose(file); // Close the file before exiting
+        fclose(file);
         return 1; // Return an error code
     }
 
