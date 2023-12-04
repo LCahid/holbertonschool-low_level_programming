db3a803e13ffab82ce301138a2786ec99e23edb4
Cahid <mr.cebizade@gmail.com>
2023-12-04 07:13:44 -0800

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,13 +13,15 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp;
 	
-	if (!*head)
 	temp = malloc(sizeof(list_t));
 	if (!temp)
 		return (NULL);
 	temp->str = strdup(str);
 	temp->len = strlen(str);
-	temp->next = *head;
-	*head = temp;
+	temp->next = NULL;
+	if (*head)
+		*head->next = temp;
+	else
+		*head = temp;
 	return (temp);
 }
