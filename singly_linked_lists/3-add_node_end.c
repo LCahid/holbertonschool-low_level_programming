1c741990ba29d7c7ca1f9312e2e3c8e3fb6dc53b
Cahid <mr.cebizade@gmail.com>
2023-12-04 07:21:14 -0800

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,13 +6,13 @@
 /**
   * add_node_end - adding new node to the list
   * @head: start of list
-  * @str: new data 
+  * @str: new data
   * Return: new list
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp, *tmp;
-	
+
 	temp = malloc(sizeof(list_t));
 	if (!temp)
 		return (NULL);
