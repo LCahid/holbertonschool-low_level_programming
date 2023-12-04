e4b025bc4862753f3aa9ecb94604c507c30dedf8
Cahid <mr.cebizade@gmail.com>
2023-12-04 07:03:49 -0800

    2

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,15 +4,16 @@
 #include "lists.h"
 
 /**
-  * add_node - adding new node to the list
+  * add_node_end - adding new node to the list
   * @head: start of list
-  * @str: new data
+  * @str: new data 
   * Return: new list
   */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp;
-
+	
+	if (!*head)
 	temp = malloc(sizeof(list_t));
 	if (!temp)
 		return (NULL);
