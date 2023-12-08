1a8a1a076a4d5fa509d05420362f8d1b8515c4ba
Cahid <mr.cebizade@gmail.com>
2023-12-07 16:15:42 -0800

    10

--- bit_manipulation/2-main.c
+++ bit_manipulation/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-
+#include <limits.h>
 /**
  * main - check the code
  *
@@ -16,5 +16,7 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
+	n = get_bit(ULONG_MAX, 20000);
+	printf("%d\n", n);
     return (0);
 }
