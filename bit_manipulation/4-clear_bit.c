078881ac3b09dd774da120e2253523c63d143c98
Cahid <mr.cebizade@gmail.com>
2023-12-07 23:53:06 -0800

    100

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,16 +1,17 @@
 /**
-  * set_bit - sets the value of a bit to 1 at given index
+  * clear_bit - sets the value of a bit to 0 at given index
   * @n: number
   * @index: index of bit which this function returns
   * Return: the value of a bit at a given index. if error ocured -1
   */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int k = 1;
 
 	if (index > 31)
 		return (-1);
-	k = k << index;
-	*n |= k;
+	k <<= index;
+	k = ~k;
+	*n &= k;
 	return (1);
 }
