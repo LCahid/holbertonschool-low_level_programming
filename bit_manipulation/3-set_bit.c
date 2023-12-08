8c5c2ec2d5613cce465da69384e75cc43282f387
Cahid <mr.cebizade@gmail.com>
2023-12-07 23:15:26 -0800

    11

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+/**
+  * set_bit - sets the value of a bit to 1 at given index
+  * @n: number
+  * @index: index of bit which this function returns
+  * Return: the value of a bit at a given index. if error ocured -1
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int k = 1, i;
+
+	if (index > 31)
+		return (-1);
+	for (i = 0; i < index; i++)
+		k = k << 1;
+	*n |= k;
+	return (1);
+}
