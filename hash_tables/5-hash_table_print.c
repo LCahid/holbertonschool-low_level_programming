ce96e6b3ca68eff9ae3afe87487dfbbb1db8a785
Cahid <mr.cebizade@gmail.com>
2023-12-16 07:12:22 -0800

    5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -3,7 +3,7 @@
 #include "hash_tables.h"
 #include <string.h>
 /**
-  * hash_table_print - retrieves a value associated with a key. 
+  * hash_table_print - retrieves a value associated with a key.
   * @ht: hash_table
   * Return:  actual number of letters it could read and print
   */
