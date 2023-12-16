15771b2b6eedddb28e244177bb23b4714d351c9c
Cahid <mr.cebizade@gmail.com>
2023-12-16 02:57:21 -0800

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *t = malloc(sizeof(hash_table_t));
 
 	t->size = size;
-	t->array = malloc(sizeof(int *) * size);
+	t->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(t->array))
 		return (NULL);
 	return (t);
